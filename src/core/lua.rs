678c0ddbdaf07bfbaf174363e9cb386b3a641c7a
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-12-07 16:00:43 -0400

    feat(lua): add `log` table to bridge Rust tracing with Lua

--- src/core/lua.rs
+++ src/core/lua.rs
@@ -1,9 +1,41 @@
 use mlua::prelude::{LuaResult, LuaTable};
 use mlua::Lua;
 
+use tracing::{info, warn, error, debug, trace};
+
 const NEOSH_STDLIB: &str = include_str!("../lua/neosh.lua");
 
-// Initialize Lua globals
+fn info(_: &Lua, msg: String) -> LuaResult<()> {
+    let msg = msg.as_str();
+    info!(msg);
+    Ok(())
+}
+
+fn warn(_: &Lua, msg: String) -> LuaResult<()> {
+    let msg = msg.as_str();
+    warn!(msg);
+    Ok(())
+}
+
+fn error(_: &Lua, msg: String) -> LuaResult<()> {
+    let msg = msg.as_str();
+    error!(msg);
+    Ok(())
+}
+
+fn debug(_: &Lua, msg: String) -> LuaResult<()> {
+    let msg = msg.as_str();
+    debug!(msg);
+    Ok(())
+}
+
+fn trace(_: &Lua, msg: String) -> LuaResult<()> {
+    let msg = msg.as_str();
+    trace!(msg);
+    Ok(())
+}
+
+// Initialize Lua globals and logging
 pub fn init(lua: &Lua) -> LuaResult<LuaTable> {
     // ===== Setup package path so we can require scripts
     lua.load(
@@ -25,5 +57,15 @@ pub fn init(lua: &Lua) -> LuaResult<LuaTable> {
     globals.set("neosh", lua_neosh)?;
 
     lua.load(NEOSH_STDLIB).set_name("neosh")?.exec()?;
+
+    // ===== Set logging functions
+    let lua_log = lua.create_table()?;
+    lua_log.set("info", lua.create_function(info)?)?;
+    lua_log.set("warn", lua.create_function(warn)?)?;
+    lua_log.set("error", lua.create_function(error)?)?;
+    lua_log.set("debug", lua.create_function(debug)?)?;
+    lua_log.set("trace", lua.create_function(trace)?)?;
+    globals.set("log", lua_log)?;
+
     Ok(globals)
 }
