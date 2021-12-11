e06096666f79ec67c4019b5ecd254b34f15f3caf
shift-d <shiftkeydeveloper@gmail.com>
2021-12-11 15:01:48 +0300

    chore(docs): add docs for log.rs

--- src/log.rs
+++ src/log.rs
@@ -1,16 +1,22 @@
 use std::path::PathBuf;
 use tracing_appender::non_blocking::WorkerGuard;
-
 use tracing_subscriber::{fmt::Subscriber, EnvFilter};
-/// Set up tracing stuff
+
+/// Set up log system
+///
+/// Create file writer and merge it with tracing's subscriber
 pub fn setup(data_dir: &PathBuf) -> WorkerGuard {
+    // File writer
     let appender = tracing_appender::rolling::never(data_dir, "neosh.log");
     let (non_blocking_appender, guard) = tracing_appender::non_blocking(appender);
+    // Subscriber
     let subscriber = Subscriber::builder()
         .with_ansi(false)
         .with_env_filter(EnvFilter::new("neosh=debug"))
         .with_writer(non_blocking_appender)
         .finish();
+
+    // Set up subscriber as global
     tracing::subscriber::set_global_default(subscriber).unwrap();
     tracing::info!("Tracing is running");
 
@@ -18,8 +24,10 @@ pub fn setup(data_dir: &PathBuf) -> WorkerGuard {
 }
 
 pub mod utils {
+    //! Reusable log messages
     use tracing::debug;
 
+    /// Log message for built-in NeoSH commands
     pub fn command(name: &str) {
         debug!("Executing built-in `{}` command", name);
     }
